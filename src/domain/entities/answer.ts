4d9f34a0151c86812ef2a623f9a433c5a6152cd6
gabrielLaino <lainogabriel14@gmail.com>
2026-07-01 21:57:11 -0300

    feat: criando outras propriedades para minhas entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 
 interface AnswerProps {
     content: string,
-    instructorId: string,
-    questionId: string, 
+    instructorId: UniqueEntityId,
+    questionId: UniqueEntityId, 
+    createdAt: Date,
+    updatedAt?: Date,
 }
 
 export class Answer extends Entity<AnswerProps> {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Slug } from "./values-objects/slug";
 import { Entity } from "@/core/entities/entity";
 
 interface QuestionProps {
   title: string, 
   content: string,
-  authorId: string,
+  authorId: UniqueEntityId,
+  bestAnswerId?: UniqueEntityId,
   sub: Slug,
+  createdAt: Date,
+  updatedAt?: Date,
 }
 
 export class Question extends Entity<QuestionProps> {}
